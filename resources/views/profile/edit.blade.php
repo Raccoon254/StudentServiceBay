9c7a05ad3741b889e59a2c97d251a6f68829e76e
Steve Tom <tomsteve187@gmail.com>
2023-11-01 15:38:13 +0300

    User.Edit View fix

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,56 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+
+
+        <div class="max-w-7xl bg-gray-500 bg-opacity-30 backdrop-blur-sm p-6 rounded-md mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <div class="flex">
+                <div class="w-1/2">
+                    <img src="{{ asset("/storage/profile_photos/".$user->profile_photo) }}" alt="{{ $user->name }}" class="h-60 w-60 rounded-full flex items-center justify-center">
+                </div>
+                <div class="w-1/2">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
+            <div class="flex">
+                <div class="flex gap-3">
+                    <div class="w-1/2">
+                        <section>
+                            <header>
+                                <h2 class="text-lg font-medium text-gray-300">
+                                    {{ __('Profile Information') }}
+                                </h2>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+                                <p class="mt-1 text-sm text-gray-400">
+                                    {{ __("Below is your Full data inStudent Service Bay. We protect your data fro privacy") }}
+                                </p>
+                            </header>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                            <div class="mt-6">
+                                <x-input-label for="last_name" :value="__('Name')" />
+                                <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->name)"  />
+                            </div>
+
+                            <div class="mt-6">
+                                <x-input-label for="email" :value="__('Email')" />
+                                <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)"  />
+                            </div>
+
+                            <div class="mt-6">
+                                <x-input-label for="phone" :value="__('Phone')" />
+                                <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full" :value="old('phone', $user->phone_number)"  />
+                            </div>
+
+                           </section>
+
+                    </div>
+
+                    <div class="w-1/2">
+                        @include('profile.partials.update-password-form')
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </x-app-layout>
