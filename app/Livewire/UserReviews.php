5928bfab424fb503fb8209b9e78393e08414c9a4
Steve Tom <tomsteve187@gmail.com>
2023-11-02 08:29:57 +0300

    Added User review

---
+++ app/Livewire/UserReviews.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UserReviews extends Component
+{
+    public function render()
+    {
+        return view('livewire.user-reviews');
+    }
+}
